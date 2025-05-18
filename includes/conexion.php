42fb237f3e8b37831bfe8788326e5b71d47a55f1
zamukay <133037731+zamukay@users.noreply.github.com>
2025-05-18 10:50:00 -0500

    conexion con postgres

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,10 +1,9 @@
 <?php
-    // Parámetros de conexión
-    $host = "localhost";       // Dirección del servidor PostgreSQL
-    $port = "5432";            // Puerto por defecto
-    $dbname = "nombre_base";   // Reemplaza con el nombre de tu base de datos
-    $user = "postgres";        // Nombre de usuario (por defecto suele ser postgres)
-    $password = "tu_clave";    // Reemplaza con tu contraseña
+    $host = "localhost";       
+    $port = "5432";           
+    $dbname = "gestion_cursos";   
+    $user = "postgres";        
+    $password = "ligacampeon";    
 
     // Cadena de conexión
     $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
@@ -12,11 +11,11 @@
     // Intentar conexión
     $conexion = pg_connect($conn_string);
 
-    /*
+    
     if (!$conexion) {
         echo "Error: No se pudo conectar a la base de datos.";
     } else {
         echo "Conexión exitosa a PostgreSQL.";
     }
-    */
+    
 ?>
